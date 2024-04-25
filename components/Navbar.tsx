864009c22bde39f915db37ff76a55e802e3aac39
shiyammm <122890678+shiyammm@users.noreply.github.com>
2024-04-25 23:14:24 +0530

    Add SignIn button to Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import MobileNav from './MobileNav';
+import { SignedIn, UserButton } from '@clerk/nextjs';
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1 ">
         <Image
           src="/icons/logo.svg"
@@ -17,10 +18,13 @@ const Navbar = () => {
         <p className="text-[26px] font-extrabold text-white max-sm:hidden">
           Link Up
         </p>
-        <div className="flex-between gap-5">
-          <MobileNav />
-        </div>
       </Link>
+      <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton/>
+        </SignedIn>
+        <MobileNav />
+      </div>
     </nav>
   );
 };
